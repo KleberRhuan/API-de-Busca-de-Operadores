655af2d9227694931d3a8348bd1329c88199da33
KleberRhuan <kleber_rhuan@hotmail.com>
2025-03-31 06:09:04 -0300

    chore:  add health check route and changed redis configs.

--- src/infra/config/config.py
+++ src/infra/config/config.py
@@ -1,11 +1,14 @@
 import os
+
 from fastapi import FastAPI
+from limits.storage import MemoryStorage, RedisStorage
+
 from src.infra.config.swagger_config import configure_swagger
-from src.presentation.exception.exception_handlers import register_exception_handlers
+from src.infra.database import get_redis_connection
 from src.infra.middleware.cors_middleware import setup_cors
 from src.infra.middleware.rate_limit_middleware import RateLimitMiddleware
-from limits.storage import RedisStorage, MemoryStorage
-from src.infra.database import get_redis_connection
+from src.presentation.exception.exception_handlers import \
+    register_exception_handlers
 
 
 def get_current_env():
@@ -13,29 +16,30 @@ def get_current_env():
 
 
 def get_app_url():
-    return os.getenv("APP_URL", "http://127.0.0.1:8080")
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 8080))
+    return f"http://{host}:{port}"
 
 
 class Config:
     orm_mode = True
     allow_population_by_field_name = True
-    
+
     @staticmethod
     def app_url():
         return get_app_url()
 
-
 def setup_application(app: FastAPI) -> FastAPI:
     # Configurar CORS
     setup_cors(app)
-    
+
     # Configurar rate limiter
     rate_limit = int(os.getenv("RATE_LIMIT", "100"))
     rate_window = int(os.getenv("RATE_WINDOW", "60"))
-    
+
     # Obter conexão Redis compartilhada do módulo database
     redis_instance = get_redis_connection()
-    
+
     # Escolher o armazenamento para o rate limiter
     if redis_instance:
         try:
@@ -44,19 +48,16 @@ def setup_application(app: FastAPI) -> FastAPI:
             storage = MemoryStorage()
     else:
         storage = MemoryStorage()
-    
+
     # Adicionar o middleware de rate limiting
     app.add_middleware(
-        RateLimitMiddleware,
-        limit=rate_limit,
-        window=rate_window,
-        storage=storage
+        RateLimitMiddleware, limit=rate_limit, window=rate_window, storage=storage
     )
-    
+
     # Configurar Swagger
     configure_swagger(app)
-    
+
     # Registrar tratadores de exceção
     register_exception_handlers(app)
-    
+
     return app
--- src/presentation/api/routes.py
+++ src/presentation/api/routes.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
-from src.infra.database import get_cache, get_db
-from src.presentation.model.operator_request_params import OperatorRequestParams
-from src.infra.config.swagger_config import ENDPOINT_CONFIG as SWAGGER_CONFIG
-from src.application.service.operator_service import OperatorService
+from aiocache import Cache, caches
 import src.infra.config as config
+from src.application.service.operator_service import OperatorService
+from src.infra.config.swagger_config import ENDPOINT_CONFIG as SWAGGER_CONFIG
+from src.infra.database import get_db
+from src.presentation.model.operator_request_params import \
+    OperatorRequestParams
 
 api_router = APIRouter(prefix="/api/v1")
 
 # Função para injeção de dependência do OperatorService
 def get_operator_service(db=Depends(get_db)):
     return OperatorService(db)
+
+@api_router.get("/api/v1/health")
+def health():
+    return {"status": "ok"}
 
 @api_router.get(
     "/operators",
@@ -19,7 +25,7 @@ def get_operator_service(db=Depends(get_db)):
     summary=SWAGGER_CONFIG["operators"]["summary"],
     description=SWAGGER_CONFIG["operators"]["description"],
     response_description=SWAGGER_CONFIG["operators"]["response_description"],
-    responses=SWAGGER_CONFIG["operators"]["responses"]
+    responses=SWAGGER_CONFIG["operators"]["responses"],
 )
 async def search_operators(
     params: OperatorRequestParams = Depends(),
@@ -27,24 +33,27 @@ async def search_operators(
 ):
     return await operator_service.find_all_cached(params)
 
+
 # Endpoint condicional para ambiente de desenvolvimento
 if config.get_current_env() == "dev":
+
     @api_router.get(
-        "/cache-tests", 
+        "/cache-tests",
         response_class=JSONResponse,
         tags=[SWAGGER_CONFIG["cache_test"]["tag"]],
         summary=SWAGGER_CONFIG["cache_test"]["summary"],
         description=SWAGGER_CONFIG["cache_test"]["description"],
-        responses=SWAGGER_CONFIG["cache_test"]["responses"]
+        responses=SWAGGER_CONFIG["cache_test"]["responses"],
     )
     async def cache_test():
-        cache = get_cache()
+        cache = caches.get("default")
         key = "cache_test_key"
         test_value = {"message": "Cache is working!"}
 
         await cache.set(key, test_value, ttl=60)
         cached_value = await cache.get(key)
         return {"status": "success", "cached_value": cached_value}
+
 
 def get_router():
     return api_router
